984ec4a5d8c9e98a7eca2bc7e435e275d7ae7791
95Caique <decaiquesouza@gmail.com>
2024-12-11 16:48:20 -0300

    Adição de autententicação via jwt

--- pessoa/settings.py
+++ pessoa/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'cadastro',
+    'autenticação',
 ]
 
 MIDDLEWARE = [
@@ -89,6 +90,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTauthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.Isauthenticated',
+    )
+}
 
 
 # Internationalization
